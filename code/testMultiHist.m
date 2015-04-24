70a2391691ff397effa70f380839c62ce1fe51d6
= <gaviadler@gmail.com>
2015-04-24 12:48:18 -0400

    just structures, no svm

--- code/testMultiHist.m
+++ code/testMultiHist.m
@@ -19,60 +19,12 @@ for i=1:length(testImgNames)
         probabilityOfStructure(classind)= log(prod(prob));
     end
     
-    % Do a transformation to make the smallest negative large
-    % and the largest negatives small
-    probMin = consts.LOG_PROB_MIN;
-    probabilityOfStructure(probabilityOfStructure < probMin) = probMin;
-    probabilityOfStructure = abs(probMin) - abs(probabilityOfStructure);
-    
-    %% Selecte classes so that it represents up to .5 of the total probability
-    totalProb = sum(probabilityOfStructure);
-    probsLeft = probabilityOfStructure; % Probs we have not selected already
-    selectedProbs = 0; % Sum of selected probs
-    selectedClasses = []; % The classes we will test
-    while (selectedProbs < consts.NB_THRESH*totalProb)
-        maxProb = max(probsLeft); % Max remaining class prob
-        selectedProbs = selectedProbs + sum(maxProb); % Add to running total
-        % Add class of max prob to class list
-        classToAdd = find(probabilityOfStructure == maxProb);
-        selectedClasses = [selectedClasses classToAdd];
-        probsLeft(probsLeft == maxProb) = [];
-    end
-   
-    %% Test on SVMs
-    probabilityList=[];
-    for classInd = selectedClasses
-        classModel = model.classes{classInd};
-
-        %% Calculate feature vector
-        featureVector = computeFeatureVect(I, classModel.structure, ...
-                consts.PRUNING_DEPTH_MAX, consts.WNAME, consts.ENTROPY,...
-                consts.NUM_BINS, consts.DEBUG);
-
-        %% Apply to SVM
-        % TODO fix, is normalization handled?
-
-        
-        zeroedFV=bsxfun(@minus, featureVector ,classModel.normmin);
-        
-        
-        featureVector=bsxfun(@times,zeroedFV,1./(classModel.normmax-classModel.normmin));
-        keyboard;
-
-        svmLabel = ((testLabels(i) == classModel.label) * 2) - 1; % Convert label to +-1
-
-        [predictedLabel, accuracy, decisionValues] = svmpredict(svmLabel, featureVector', classModel.svm);
-        probabilityList = [probabilityList; decisionValues];
-
-     end
-
-    %% Pick best match
-    [~,indexInSelClasses] = max(probabilityList);
-    BestMatch=selectedClasses(indexInSelClasses);
-    C = updateCounts(C, testLabels(i), allClasses(BestMatch), allClasses);
+%     %% Pick best match
+    [~,indexInSelClasses] = max(probabilityOfStructure);
+    C = updateCounts(C, testLabels(i), allClasses(indexInSelClasses), allClasses);
     
     if consts.DEBUG
-       disp(['Guess: ', num2str(allClasses(BestMatch)), ' (', num2str(testLabels(i)), ')']);
+       disp(['Guess: ', num2str(allClasses(indexInSelClasses)), ' (', num2str(testLabels(i)), ')']);
     end
 end
 end
