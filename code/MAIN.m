622c06e557bd01f9fe2de6bd87c0442efb91f172
Spencer Barton <sbarton272@gmail.com>
2015-04-29 11:17:52 -0400

    Ran baseline

--- code/MAIN.m
+++ code/MAIN.m
@@ -5,14 +5,14 @@ close all force
 
 %% Load data
 
-% load('../data/images/traintest.mat', 'train_imagenames', 'train_labels',...
-%     'test_imagenames', 'test_labels', 'mapping');
+load('../data/images/traintest.mat', 'train_imagenames', 'train_labels',...
+     'test_imagenames', 'test_labels', 'mapping');
 
-load('../data/images/devData.mat', 'dev_imagenames', 'dev_labels', 'mapping');
-train_imagenames = dev_imagenames;
-test_imagenames = dev_imagenames;
-train_labels = dev_labels;
-test_labels = dev_labels;
+%load('../data/images/devData.mat', 'dev_imagenames', 'dev_labels', 'mapping');
+%train_imagenames = dev_imagenames;
+%test_imagenames = dev_imagenames;
+%train_labels = dev_labels;
+%test_labels = dev_labels;
 
 %% Add Libraries
 addpath('LibSvm-3.20/');
